b55e758ffb1fe27d096f4491e778428d4b5ccbba
Andrey Surkov <cookwer@gmail.com>
2014-09-21 20:06:31 +0400

    админка

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import admin.AdminPageServlet;
 import frontend.Frontend;
 import frontend.SignUpServlet;
 import org.eclipse.jetty.server.Handler;
@@ -11,23 +12,25 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 import javax.servlet.Servlet;
 
-/**
- * @author v.chibrikov
- */
 public class Main {
     public static void main(String[] args) throws Exception {
-        int port = 8000;
-        if (args.length == 1) {
-            String portString = args[0];
-            port = Integer.valueOf(portString);
+        //int port = 8000;
+        if (args.length != 1) {
+//            String portString = args[0];
+//            port = Integer.valueOf(portString);
+            System.out.append("Use port as the first argument");
+            System.exit(1);
         }
 
+        String portString = args[0];
+        int port = Integer.valueOf(portString);
         System.out.append("Starting at port: ").append(String.valueOf(port)).append('\n');
 
         Servlet frontend = new Frontend();
 
         Server server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new AdminPageServlet()), AdminPageServlet.adminPageURL);
         context.addServlet(new ServletHolder(frontend), "/api/v1/auth/signin");
         AccountService accountService = new AccountService();
         Servlet signUp = new SignUpServlet(accountService);
